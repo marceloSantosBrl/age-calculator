967022722866d7effe9492d84b2275c9ac231110
Marcelo Santos <marcelegald2@outlook.com>
2023-09-10 14:46:43 -0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InputsComponent } from './inputs/inputs.component';
+import { ButtonComponent } from './button/button.component';
+import { OutputComponent } from './output/output.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputsComponent,
+    ButtonComponent,
+    OutputComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/inputs/inputs.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inputs',
+  templateUrl: './inputs.component.html',
+  styleUrls: ['./inputs.component.scss']
+})
+export class InputsComponent {
+
+  public limitYear = new Date().getFullYear;
+
+}
